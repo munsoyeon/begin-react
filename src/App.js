c2027c4051652a81f5a8121a3e5ddceedbe733cd
munsoyeon <munsy0111@gmail.com>
2023-04-04 15:59:09 +0900

    userCallback, React.memo 등을 활용한 최적화 방법 학습

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo } from "react";
+import React, { useRef, useState, useMemo, useCallback } from "react";
 import Counter from "./Counter";
 import CreateUser from "./CreateUser";
 import Hello from "./Hello";
@@ -16,10 +16,13 @@ function App() {
     email: "",
   });
   const { username, email } = inputs;
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    setInputs({ ...inputs, [name]: value });
-  };
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setInputs({ ...inputs, [name]: value });
+    },
+    [inputs]
+  ); // 참조하는 값을 두번째 depth에 넣어줘야 그 값이 바뀔때만 함수 동작
   const [users, setUsers] = useState([
     {
       id: 1,
@@ -41,29 +44,31 @@ function App() {
     },
   ]);
   const nextId = useRef(4);
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
       email,
     };
     // setUsers([...users, user]);
-    setUsers(users.concat(user));
+    // setUsers(users.concat(user));
+    setUsers((users) => users.concat(user)); //파라미터에서 최신 users를 조회하기때문에 아래 배열에서 users를 빼도됨. 결과적으로 usernamerhk email이 바뀔때만 렌더링됨.
     setInputs({ username: "", email: "" });
     console.log(nextId.current); //4
     nextId.current += 1;
-  };
+  }, [username, email]);
+  // }, [username, email, users]);
 
-  const onRemove = (id) => {
-    setUsers(users.filter((user) => user.id !== id));
-  };
-  const onToggle = (id) => {
-    setUsers(
+  const onRemove = useCallback((id) => {
+    setUsers((users) => users.filter((user) => user.id !== id));
+  }, []);
+  const onToggle = useCallback((id) => {
+    setUsers((users) =>
       users.map((user) =>
         user.id === id ? { ...user, active: !user.active } : user
       )
     );
-  }; /* 안에 있는 원소를 업데이트해야할때도 map 사용 */
+  }, []); /* 안에 있는 원소를 업데이트해야할때도 map 사용 */
   // const count = countActiveusers(users); // 이렇게만 작성하면 input에 입력할때 계속해서 기능이 돌아감.
   const count = useMemo(() => countActiveusers(users), [users]); //users가 바뀔때에만 호출이 되고 아니면 이전것을 재사용한다.
   return (
--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 
-function User({ user, onRemove, onToggle }) {
+const User = React.memo(function User({ user, onRemove, onToggle }) {
   // useEffect(() => {
   //   console.log("컴포넌트가 나타남");
   //   // props -> state
@@ -34,7 +34,7 @@ function User({ user, onRemove, onToggle }) {
       <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
-}
+});
 function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
@@ -50,4 +50,7 @@ function UserList({ users, onRemove, onToggle }) {
   );
 }
 
-export default UserList;
+export default React.memo(
+  UserList,
+  (prevProps, nextProps) => nextProps.users === prevProps.users
+);
